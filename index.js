36ad2d42fbc053ed91d7ed676bc8d02b971dadea
Mikhail B <wproxym@at.gmail>
2020-08-18 12:03:43 +0300

    deps clarification

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-﻿var async = require('./src/async');
-var await = require('./src/await');
+﻿var async = require('./src/async'); // fiber block definder
+var await = require('./src/await'); // regular promise a+ waiter (within fiber)
 
-exports.async = async;
-exports.await = await;
+var dwait = require('./synchronize').await; // defer callback waiter (within fiber)
+var defer = require('./synchronize').defer; // defer callback anchor (within dwait)
+
+exports.async = async; // base block
+exports.await = await; // regular waiter within block
+exports.dwait = dwait;
+exports.defer = defer;
