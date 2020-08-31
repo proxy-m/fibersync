74d20fd46755acbf207a39c77dd02c426cf39109
Mikhail B <wproxym@at.gmail>
2020-08-31 08:04:23 +0300

    export synchronize-bdd test helpers as asyncBdd

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var await = require('./src/await'); // regular promise a+ waiter (within fiber)
 var dwait = require('./synchronize').await; // defer callback waiter (within fiber)
 var defer = require('./synchronize').defer; // defer callback anchor (within dwait)
 
+var asyncBdd = require('./synchronize-bdd'); // asynchronous bdd testing helpers (fiber wrappers for mocha and jasmine tests)
+
 exports.async = async; // base block
 exports.await = await; // regular waiter within block
 exports.dwait = dwait;
 exports.defer = defer;
+exports.asyncBdd = asyncBdd;
